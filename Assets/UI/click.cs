f3b499a21c63086718299a1f803361285e83118e
azliayuu <azliayu@alumni.pcr.ac.id>
2021-07-02 16:43:52 +0700

    ARprojek

---
+++ Assets/UI/click.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour
+{
+   void OnMouseDown() {
+		scale.ScaleTransform = this.transform;
+	}
+
+}
